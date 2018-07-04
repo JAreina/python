31291ac4f94037dc6398afe0f1314c6e969f175e
JAreina <jareinafdez@gmail.com>
2018-07-04 18:30:31 +0200

    tuplas

---
+++ 1-py/venv/8_tuplas.py
@@ -0,0 +1,21 @@
+'''
+tuplas : no puede cambiar de valores
+'''
+
+tupla = (12,22,33)
+print(tupla[0])
+print(tupla[1])
+print(tupla[2])
+
+tupla2 = ('a','b','c')
+print(tupla2[0])
+print(tupla2[1])
+print(tupla2[2])
+
+tupla3= (1, 'tupla',2.2, False)
+print(tupla3[0])
+print(tupla3[1])
+print(tupla3[2])
+print(tupla3[3])
+
+#tupla[0] = 22 #error no se puede asignar nuevaente
