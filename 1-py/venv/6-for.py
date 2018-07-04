cba77d89cf5e3a56d8d621184acb3e5974e95ccc
JAreina <jareinafdez@gmail.com>
2018-07-04 17:28:33 +0200

    for

---
+++ 1-py/venv/6-for.py
@@ -0,0 +1,27 @@
+a = 100
+
+for a in range(100,105) :
+    print(a)
+
+print("------------")
+
+
+for a in range(100,110,2) :  #de dos en dos
+    print(a)
+
+print("------------")
+for a in range(110,100,-1) :  #reverse
+    print(a)
+
+print("------------")
+
+cadena = "en un lugar de la Mancha"
+
+for letra in cadena :
+    print(letra)
+
+
+b = ['palabra',["dos"]], ["tres"]
+
+for i in b :
+    print(i)
