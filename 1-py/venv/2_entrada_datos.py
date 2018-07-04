012873c702479379dba36639b60ef6981d987db8
JAreina <jareinafdez@gmail.com>
2018-07-04 16:30:58 +0200

    if

---
+++ 1-py/venv/2_entrada_datos.py
@@ -0,0 +1,9 @@
+'''
+inputs de datos
+'''
+#casting a entero del dato recibido
+entrada = int(input('DI UN NÚMERO\n'))
+print(entrada * 100)
+
+
+
---
+++ 1-py/venv/if.py
@@ -0,0 +1,12 @@
+'''
+inputs de datos
+'''
+#casting a entero del dato recibido
+entrada = int(input('DI UN NÚMERO\n'))
+entrada2 = int(input('DI OTRO NÚMERO\n'))
+
+if entrada > entrada2 :
+
+    print('{} es mayor '.format(entrada) )
+else :
+     print('{} es mayor'.format(entrada2))
