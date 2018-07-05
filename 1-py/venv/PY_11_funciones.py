7616010fb3dd08a44c0817a2504d919aefaf265d
JAreina <jareinafdez@gmail.com>
2018-07-05 17:20:48 +0200

    import, from , def

---
+++ 1-py/venv/PY_11_funciones.py
@@ -0,0 +1,13 @@
+import random
+from math import factorial
+
+aleatorio = random.randint(100,1000)
+print(aleatorio, "ALEATORIO")
+
+factor = factorial(5)
+print(factor)
+
+def mi_funcion():
+    print("es una funcion")
+
+mi_funcion()
