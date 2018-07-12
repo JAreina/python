89a2e4dfc772b2897e663ecfb8cc48f09082c7b6
JAreina <jareinafdez@gmail.com>
2018-07-12 19:30:59 +0200

    ficheros json leer

--- 1-py/venv/PY_18_ARCHIVOS_JSON.py
+++ 1-py/venv/PY_18_ARCHIVOS_JSON.py
@@ -3,7 +3,12 @@ import json
 ARCHIVOS JSON
 '''
 
-
+'''
+con esto guardalo como file
+'''
 with open("archivo.json") as file:
     dato = json.load(file)
 print(dato)
+print("......\n")
+
+print(dato['members'][0]['powers'][0])
