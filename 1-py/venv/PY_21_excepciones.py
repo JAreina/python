c0dd89ca41fd9c8f2b79fbd8e6a5225893a89836
JAreina <jareinafdez@gmail.com>
2018-07-13 18:27:27 +0200

    Exception

--- 1-py/venv/PY_21_excepciones.py
+++ 1-py/venv/PY_21_excepciones.py
@@ -4,11 +4,28 @@ try :
 
     print(listaa[100])
 except IndexError:
-    print("error en el indice de la lista")
-    try :
-        print(listaa[0] / 0)
-
-    except ZeroDivisionError:
-        print("division por cero")
-finally:
+        print("error en el indice de la lista")
+        try :
+            #print(listaa[0] / 2)
+             print(listaa[0]/ 0)
+        except ZeroDivisionError:
+            print("division por cero")
+        else:
+            print("hola soy else ") # si sale bien el try de la division se ejecuta
+finally :
     print("finalmente")
+
+
+# HEREDA DE Exception
+
+class MiExcepcion(Exception):
+    def __init__ (self,x):
+        print("ERROR DICE: ",x)
+
+try :
+    if 4 == 4:
+     raise MiExcepcion("ERROR LANZADO")
+except MiExcepcion:
+    print("error")
+
+
