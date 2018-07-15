c0ca600d1bcf50d847724ece8f71b5e9247ea6a7
JAreina <jareinafdez@gmail.com>
2018-07-15 17:53:55 +0200

    request headers

--- 2-py/venv/proy.py
+++ 2-py/venv/proy.py
@@ -1,9 +1,25 @@
 import urllib.request as request
+import os
 # VERIFICAR HTTPS
 
 req = request.Request('http://python.org')
 resultado = request.urlopen(req)
 
 print(resultado.geturl())
+resultado.close()
 
 
+# peso pagina  de los headers de una petición
+
+
+url2 = "https://python.org"
+sitio = request.urlopen(url2)
+meta = sitio.info()
+print(meta)
+print("content-Length", int(sitio.headers['content-length'])/1024)
+
+archivo = open("http.txt","w")
+archivo.write(str(meta) )
+archivo.close()
+sitio.close()
+print("tamaño archivo 'http.txt': ",os.stat("http.txt").st_size)
