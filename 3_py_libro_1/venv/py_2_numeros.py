e858196b16894ebb41ec7a389416f9760e323b76
JAreina <jareinafdez@gmail.com>
2018-07-18 20:08:55 +0200

    numeros

---
+++ 3_py_libro_1/venv/py_2_numeros.py
@@ -0,0 +1,18 @@
+'''
+Integers or simply, int
+float
+complex
+'''
+
+entero = 100000
+flotante = 0.8723
+
+print("RESULTADO SIEMPRE FLOTANTE MÁS PRECISO\n")
+print("entero * flotante",entero * flotante)
+print("entero + flotante", entero + flotante)
+
+a = b = 10.10
+a = a - .10
+
+print(a , id(a))
+print(b, id(b))
