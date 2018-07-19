6845bcd687803b09c2e84ede015acb64b4a59e76
JAreina <jareinafdez@gmail.com>
2018-07-19 18:38:31 +0200

    operadores

--- 3_py_libro_1/venv/py_7_operadores.py
+++ 3_py_libro_1/venv/py_7_operadores.py
@@ -5,3 +5,27 @@ print( 3.0 % 2)
 print("DIVISION" ,3 / 2)
 print("floor division", 3 // 2 ) # redondea hacia abajo
 print(2 ** 2)
+
+'''
+prioridad operadores 
+
+parentesis
+exponentes,
+multiplicacion
+division
+suma
+resta
+'''
+
+a  = 2 + 2 * 5
+print(a)
+a  = (2 + 2) * 5
+print(a)
+
+
+print (5 - 6 * 2)
+print ((5 - 6) * 2)
+
+
+print (3 ** 3 * 5) #135
+print (3** (3 * 5)) # 14348907
