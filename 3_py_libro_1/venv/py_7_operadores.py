2ac10776018df0da9a3ed0f9d492942b1e2352f0
JAreina <jareinafdez@gmail.com>
2018-07-19 18:55:56 +0200

    operadores

--- 3_py_libro_1/venv/py_7_operadores.py
+++ 3_py_libro_1/venv/py_7_operadores.py
@@ -1,4 +1,8 @@
 
+'''
+
+MATEMATICOS
+'''
 
 print( 3 % 2)
 print( 3.0 % 2)
@@ -29,3 +33,39 @@ print ((5 - 6) * 2)
 
 print (3 ** 3 * 5) #135
 print (3** (3 * 5)) # 14348907
+
+
+'''
+OPERADORES COMPARACION
+'''
+
+
+z = 5
+y = 5
+
+print ( z == y)
+print( 10 == 10.0)
+
+print( z != y )
+#print( z <> y ) no soportado version 3.5
+
+print( z > y )
+print( z >= y )
+
+
+z += 100
+print( z )
+
+
+
+'''
+OPERADORES LOGICOS
+'''
+print("OPERADORES LOGICOS \n")
+print(5 == 5 and 10 == 10) # true
+print(5 == 5 and 7 < 6) # false
+print(5 == 5 and 7 == 7 and 6 < 7) #v
+print(5 == 5 or 7 < 6) # v
+print(5 == 5 or 7 < 6 or 5 >10)##v
+print(not (5 == 5))#f
+print(not (5 == 6))#v
