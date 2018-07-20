c3ee111388c773e32900aee2b157e3f29df0dfb9
JAreina <jareinafdez@gmail.com>
2018-07-20 19:28:43 +0200

    math

---
+++ 3_py_libro_1/venv/py_12_math.py
@@ -0,0 +1,16 @@
+import math
+
+a = 10
+b = 100
+print(a - b )
+print(abs(a -b ))
+
+print('\n ceil , floor \n ')
+c = 10.5
+d = 9.2
+print (math.ceil (c))
+print (math.floor(d))
+
+print('\n pow \n ')
+print (pow (3,4))
+print ( 3 ** 4 )
