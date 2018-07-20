6bc2a0bdc0b4ad32243d11aee6f0e5811170ddbc
JAreina <jareinafdez@gmail.com>
2018-07-20 07:55:32 +0200

    while

---
+++ 3_py_libro_1/venv/py_9_while.py
@@ -0,0 +1,27 @@
+x = 0
+while (x < 25):
+ print ("The value of x: ", x)
+ x = x+1
+
+counter=100
+while (counter > 0):
+
+    print (counter)
+    counter=counter - 10
+
+
+y = 0
+while (y >= 0):
+    if y == 1000:
+        break
+    print (y)
+    y = y+1
+
+
+
+counter = 100
+while (counter > 0):
+ print (counter)
+ counter = counter - 10
+else:
+ print('Y is no longer greater than zero')
