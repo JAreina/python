b59572ed64067a4a777cbde5ff80b1b68f6477ae
JAreina <jareinafdez@gmail.com>
2018-07-23 09:32:26 +0200

    time, calendar

---
+++ 3_py_libro_1/venv/py_18_time_date/py_18_time_calendar_2.py
@@ -0,0 +1,22 @@
+import calendar
+cal=calendar.month(2018, 7)
+print(cal)
+
+
+print("\n funciones \n")
+
+
+a = calendar.firstweekday()
+print(a)
+
+print("\n año bisiesto\n")
+
+print(calendar.isleap(2018))
+
+calendar.setfirstweekday(1)
+print(calendar.firstweekday())
+
+print(calendar.weekday(2018,5,23))
+
+
+#   http://docs.python.org.
--- 3_py_libro_1/venv/py_18_time_date/py_18_time_date_1.py
+++ 3_py_libro_1/venv/py_18_time_date/py_18_time_date_1.py
@@ -4,3 +4,24 @@ tiempo_local = time.localtime() # return una tupla
 print(tiempo_local)
 
 print(tiempo_local.tm_year)
+
+
+
+
+localtime=time.localtime(time.time())
+print(localtime)
+
+
+print("\n   formato humano\n")
+
+formattedtime=time.asctime(time.localtime(time.time()))
+print(formattedtime)
+
+
+print("\n funciones time \n")
+
+print(time.clock()) # reloj del pc
+
+
+print("\n actual GMT \n")
+print(time.gmtime(time.time()))
