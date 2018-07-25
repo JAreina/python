44265ed3ac326ef94489b6804206f1b56bdbd9e1
JAreina <jareinafdez@gmail.com>
2018-07-25 18:58:34 +0200

    sets

--- 4_py_libro_2/venv/py_6_sets.py
+++ 4_py_libro_2/venv/py_6_sets.py
@@ -12,6 +12,29 @@ se puede usar in para indagar la existencia en el conjunto de un objeto
 a = set([1,2,3,4,1,2,3,4])
 print(a)
 
+a.pop()
+print(a)
+
+a.add(1000)
+print(a)
+
+b = set([1,2])
+print("diferencia",a.difference(b))
+
+
+c = a.union(b)
+print(c)
+
+
+print("__str__ :" ,a.__str__())
+
+d = a.symmetric_difference(b)
+print(d)
+
+
+a.clear()
+print(a)
+
 print(7 in a)
 
 #JAreina
