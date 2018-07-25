67a5773555e33e7de7244a7a5dbace85952a9c27
JAreina <jareinafdez@gmail.com>
2018-07-25 18:51:27 +0200

    sets

---
+++ 4_py_libro_2/venv/py_6_sets.py
@@ -0,0 +1,15 @@
+'''
+
+desordenado conjunto  de objetos
+de claves sin valores
+
+no admite duplicados
+
+se puede usar in para indagar la existencia en el conjunto de un objeto
+
+'''
+
+a = set([1,2,3,4,1,2,3,4])
+print(a)
+
+print(7 in a)
