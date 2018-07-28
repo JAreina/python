cbbf83bea7d3d37e9f1b43e14503303f3e29c245
JAreina <jareinafdez@gmail.com>
2018-07-28 20:08:35 +0200

    numeros

--- 4_py_libro_2/venv/py_1_tipos_datos_numeros.py
+++ 4_py_libro_2/venv/py_1_tipos_datos_numeros.py
@@ -33,3 +33,70 @@ h = f * 2
 print(h)
 i = g * 2
 print(i)
+
+print("\n ............\n")
+
+print( 5 + 2 - 3 * 2)
+#1
+print( 5 / 2 ) # floating point result with normal division
+#2.5
+print( 5 / 2.0) # also a floating point result
+#2.5
+print(5 // 2 )# integer result with truncation when divided using '//'
+#2
+print(30000000000) # This would be too large to be an int in many languages
+#30000000000
+print(30000000000 * 3)
+#90000000000
+print(30000000000 * 3.0)
+#90000000000.0
+print(2.0e-8) # Scientific notation gives back a float
+#2e-08
+print(3000000 * 3000000)
+#9000000000000
+int(200.2)
+#200
+int(2e2)
+#200
+float(200)
+
+
+print("   FUNCIONES ")
+'''
+abs, divmod, cmp, coerce, float, hex, int, long, max, min, oct,
+pow, round
+'''
+
+print( "funciones modulo math")
+
+'''
+The math module provides the following functions and constants:
+
+acos, asin, atan, atan2, ceil, cos, cosh, e, exp, fabs, floor, fmod,
+frexp, hypot, ldexp, log, log10, mod, pi, pow, sin, sinh, sqrt, tan,
+tanh
+
+
+----------------
+advanced numeric operations:  NumPy
+ www.scipy.org
+ 
+ 
+'''
+
+
+print( "   COMPLEX NUMBERS")
+
+
+'''
+Complex numbers are created automatically whenever an expression of the form nj is
+encountered, with n having the same form as a Python integer or float. j is, of course,
+standard engineering notation for the imaginary number equal to the square root of
+–1
+
+'''
+
+numeroComplejo = 232+2j
+print( numeroComplejo.real, numeroComplejo.imag)
+
+print(3 + 2j - (4+4j)) #(-1-2j)
