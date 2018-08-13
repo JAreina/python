2bf34ad02dd1aee2977237dce5dfd3d98500cadc
JAreina <jareinafdez@gmail.com>
2018-08-13 19:16:56 +0200

    sets

---
+++ 4_py_libro_2_2/venv/py_3_tuplas/py_3_tuplas_convertir_listas.py
@@ -0,0 +1,13 @@
+a = list((1, 2, 3, 4))
+
+print(a)
+#[1, 2, 3, 4]
+
+b = tuple([1, 2, 3, 4])
+print(b)
+#(1, 2, 3, 4)
+
+
+#list is a convenient way to break a string into characters:
+c = list("JAreina")
+print(c)
