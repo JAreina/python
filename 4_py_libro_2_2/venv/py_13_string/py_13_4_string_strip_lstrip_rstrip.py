28cfe6200270cc7ceddc11cb0f772874fc2ee37d
JAreina <jareinafdez@gmail.com>
2018-08-28 20:11:23 +0200

    string metodos: strip como trim

--- 4_py_libro_2_2/venv/py_13_string/py_13_4_string_strip_lstrip_rstrip.py
+++ 4_py_libro_2_2/venv/py_13_string/py_13_4_string_strip_lstrip_rstrip.py
@@ -1,3 +1,16 @@
-a = "               hola "
+import string
+
+a = "               hola \t\t"
 print(a.strip())
 print(a)
+
+print(a.rstrip())
+print(a.lstrip())
+
+print(string.whitespace)
+print(" \t\n\r\v\f")
+
+b = "aaaaabbbbbccccHOLA"
+print(b)
+print(b.strip("a"))
+print(b.strip("HOLA"))
