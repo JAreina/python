d3c1b01295385e08e86fd644509c03538863dbd6
JAreina <jareinafdez@gmail.com>
2018-09-10 19:05:07 +0200

    string metodos: format

---
+++ 4_py_libro_2_2/venv/py_13_string/py_13_10_string_metodo_format.py
@@ -0,0 +1,8 @@
+print("{0} hola{1} adios y {2}".format("JAreina",".","otra vez"))
+
+# las llaves se escapan con doble llaves {{   }}
+
+
+print("{a} xxxxxx {b}".format(a="aaaaaaaaa", b="bbbbbbbb"))
+
+print("{a} xxxxxx {b[1]}".format(a="aaaaaaaaa", b=["bbbbbbbb","cc","dd"]))
