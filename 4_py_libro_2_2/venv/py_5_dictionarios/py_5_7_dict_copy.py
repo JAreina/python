77690282480de2b409b8acbf2cbcbe13d1db5646
JAreina <jareinafdez@gmail.com>
2018-09-11 18:39:00 +0200

    dictionarios , update, contar palabras

---
+++ 4_py_libro_2_2/venv/py_5_dictionarios/py_5_7_dict_copy.py
@@ -0,0 +1,8 @@
+x = {0: 'zero', 1: 'one'}
+print("x : ", x )
+y = x.copy()
+print("y :" , y)
+#{0: 'zero', 1: 'one'}
+
+print(id(x))
+print(id(y))
---
+++ 4_py_libro_2_2/venv/py_5_dictionarios/py_5_8_dict_update.py
@@ -0,0 +1,8 @@
+# actualiza un dictionario con otro diccionario eliminando duplicados
+
+
+z = {1: 'One', 2: 'Two'}
+x = {0: 'zero', 1: 'one'}
+x.update(z)
+
+print(x)
