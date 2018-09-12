e6fcb8ab0d764627e9c890438c32477c01898b50
JAreina <jareinafdez@gmail.com>
2018-09-12 08:33:09 +0200

    for : range

--- 4_py_libro_2_2/venv/py_8_estructuras_control/py_8_4_for_range.py
+++ 4_py_libro_2_2/venv/py_8_estructuras_control/py_8_4_for_range.py
@@ -3,3 +3,17 @@ a = [1,44,-4, 0 , 99, -3, 55, -46]
 for i in range(len(a)):
     if a[i] < 0 :
         print("NEGATIVO: posicion {0} : valor: {1}".format(i, a[i]))
+
+
+
+    # rangos con dos argumentos
+
+print(list(range(44,66))) # no incluye el numero 66
+
+print(list(range(-77,-66)))
+
+print(list(range(444,66))) # lista vacia
+
+# hacia atras
+
+print(list(range(88,66,-1)))
